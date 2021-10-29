8cf4492c6086b46deafb7df19c373f0c3f91a9da
davie03p <86569871+davie03p@users.noreply.github.com>
2021-10-29 23:39:49 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -3,8 +3,8 @@
 function bottone(){
     var user = document.getElementById("user_input").value;
     var password = document.getElementById("user_password").value;
-    if (user=="davide_palleschi") {
-        if (password=="alomora03") {
+    if (user=="davide") {
+        if (password=="123456") {
             window.location.href = "./page/page1.html"
         }else{
             alert("password errata")
